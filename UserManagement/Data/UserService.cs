389ac9f0b879bcffa21e2c415a051864becebf3a
Sagar-Equitec <sagar.equitec@gmail.com>
2024-01-15 20:36:44 +0530

    Complete Blazor

---
+++ UserManagement/Data/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using UserManagement.Models;
+namespace UserManagement.Data
+{
+    public class UserService
+    {
+
+        private readonly UserContext _userContext;
+
+        public UserService(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await _userContext.Users.FromSqlRaw("EXEC GetAllUsers").ToListAsync();
+        }
+
+
+    }
+}
